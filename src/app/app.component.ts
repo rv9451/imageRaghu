c244bf197de3214c5d30f5d8a8fa3e1bcdc070c4
raghu <raghvendramishra9451@gmail.com>
2025-01-27 01:31:28 +0530

    Router fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ImageuploaderComponent } from './components/imageuploader/imageuploader.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from "./components/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,ImageuploaderComponent,FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, ImageuploaderComponent, FooterComponent, AboutComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ImageuploaderComponent } from './components/imageuploader/imageuploader.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'',
+        component:ImageuploaderComponent
+    }
+];
