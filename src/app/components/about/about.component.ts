f968f4d744f70b7364050edaa8938d20352111f0
raghu <raghvendramishra9451@gmail.com>
2025-01-28 01:23:16 +0530

    About section in progress

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  text = 'Raghvendra, Mishra!';
+  displayedText = ''; // This will hold the revealed characters
+  currentIndex = 0; // Tracks the current character index
 
+  ngOnInit() {
+    // Set an interval to display characters one by one
+    const interval = setInterval(() => {
+      if (this.currentIndex < this.text.length) {
+        this.displayedText += this.text[this.currentIndex];
+        this.currentIndex++;
+      } else {
+        clearInterval(interval); // Clear the interval once all characters are displayed
+      }
+    }, 170); // Adjust the delay (in milliseconds) as needed
+  }
 }
